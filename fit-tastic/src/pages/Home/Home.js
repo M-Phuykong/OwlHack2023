a6677cbc1f51dca8966b434aea2021416095fdee
Phuykong <m.phuykong@gmail.com>
2023-02-11 01:35:33 -0500

    setup project

---
+++ fit-tastic/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.scss';
+
+function App() {
+  return (
+    <div className="App">
+      This is Home
+      <h1>
+        This is an H1
+      </h1>
+    </div>
+  );
+}
+
+export default App;
